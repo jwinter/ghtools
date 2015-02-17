358799a6963ee36574b7c804a7e09a175bfa8702
Joe Winter <jwinter@gmail.com>
2015-02-17 17:55:43 -0500

    Add some support for fetching pull request info

--- src/ghtools/core.clj
+++ src/ghtools/core.clj
@@ -1,11 +1,29 @@
 (ns ghtools.core
-  (:require [clojure.tools.namespace.repl :refer [refresh]]))
+  (:require [clojure.tools.namespace.repl :refer [refresh]]
+            [clj-http.client :as client]
+            [cheshire.core :as json]
+            [clojure.string :as str]
+            ))
+
+(defn html-format-pulls [pulls]
+  (str/join (map (fn [pull]
+         (let [repo-name (get-in pull ["head" "repo" "full_name"])
+               pull-url (pull "url")
+               pull-title (pull "title")
+               ]
+           (str repo-name ": <a href='" pull-url "'>" pull-title "</a><br />")))
+   pulls)))
+  
 
 (defn fetch [url]
-  "")
+  (json/parse-string
+   (:body (client/get url))))
 
 (defn pulls
   "fetch pulls for this repo"
-  [api-url username repo]
-  (fetch (str api-url username repo)))
+  [username repo]
+  (fetch (str "https://api.github.com/repos/" username "/" repo "/pulls")))
 
+
+(def test-url "https://api.github.com/repos/jwinter/test-repo/pulls")
+(def sample-json (json/parse-string (:body (client/get test-url))))
--- test/ghtools/core_test.clj
+++ test/ghtools/core_test.clj
@@ -2,10 +2,17 @@
   (:require [clojure.test :refer :all]
             [ghtools.core :refer :all]))
 
+(def test-url "https://api.github.com/repos/jwinter/test-repo/pulls")
+(def sample-json (json/parse-string (:body (client/get test-url))))
+
+
 (deftest testing-pulls
   (testing "pulls"
-    (is (= ""
-           (pulls "" 'b' 'c)
-           ))
-    (is true)
-    ))
+    (let [sample-pulls (pulls "jwinter" "test-repo")]
+         (is (= 1 (count sample-pulls)))
+         (is (= "https://api.github.com/repos/jwinter/test-repo/pulls/1"
+                ((first sample-pulls) "url")))
+         (is (= "jwinter/test-repo: <a href='https://api.github.com/repos/jwinter/test-repo/pulls/1'>More detail in the README</a><br />"
+                (html-format-pulls sample-pulls)))
+         (is true)
+    )))
